309469f5d6930f497b60eeb92917c7b67ec501ca
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-10-19 18:05:57 +0200

    V1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+// Global Variable 
+const task = document.getElementById('task');
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let html = ``;
+
+for(let i = 0; i < alphabet.length; i++){
+    let content = alphabet[i];
+    html = html + `<li>${content}</li>`;
+    console.log(i);
+}
+
+console.log(html);
+
+task.innerHTML = html;
 
 
-// Iteration 3: Loops
